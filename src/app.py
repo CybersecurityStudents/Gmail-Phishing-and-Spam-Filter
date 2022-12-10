dea0ae00a41c83f3d53a10c55aa8084182d08302
Adithya Rajendran <adithyaraj@gmail.com>
2022-12-10 12:34:30 -0800

    Added message get

--- src/app.py
+++ src/app.py
@@ -6,3 +6,11 @@ API_VERSION = 'v1'
 SCOPES = ['https://mail.google.com/']
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
+emails = service.users().messages().list(userId="spamfilterbottester@gmail.com").execute()
+
+for messages in emails['messages']:
+    m = service.users().messages().get(userId="spamfilterbottester@gmail.com", id=messages['id'], format='metadata').execute()
+    print(m)
+    break
+
+s = service.users().messages().send(userId="spamfilterbottester@gmail.com")
