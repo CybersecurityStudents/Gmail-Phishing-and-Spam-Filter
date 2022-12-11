561dc8ebc0309d52857e3853010f1e4bc55410f4
hwu106 <hwu106@ucsc.edu>
2022-12-11 12:53:43 -0800

    push

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
 from Google import Create_Service
+from googleapiclient.errors import HttpError
+from datetime import datetime
+from pytz import timezone
+import pytz
 
 class Gmail_App:
     def __init__(self, first, last, email, label_name):
@@ -15,44 +19,81 @@ class Gmail_App:
         SCOPES = ['https://mail.google.com/']
         return Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
+    def test(self):
+        labels= self.service.users().labels().list(userId=self.email).execute()
+        print(labels)
+
+    def audit_log(self, type, string):
+        date_format='%m/%d/%Y %H:%M:%S %Z'
+        date = datetime.now(tz=pytz.utc)
+        date = date.astimezone(timezone('US/Pacific'))
+        fd = open("audit.txt", 'a')
+        fd.writelines(f'{date.strftime(date_format)} {type}: {string}\n')
+
     def handler(self):
-        black_lists = open('blacklist.txt', 'r')
-        bcontent = black_lists.read()
-        white_lists = open('whitelist.txt', 'r')
-        wcontent = white_lists.read()
-        emails = self.service.users().messages().list(userId=self.email).execute()
-        for messages in emails['messages']:
-            m = self.service.users().messages().get(userId=self.email, id=messages.get('id'), format='metadata').execute()
-            headers = (m.get("payload")).get("headers")
-            for header in headers:
-                if header.get("name")=="From":
-                    start = "<"
-                    end = ">"
-                    s = header.get("value")
-                    sender = s[s.find(start)+len(start):s.rfind(end)]
-                    if sender==bcontent.strip():
-                        self.add_quaratine(messages.get('id'))    
-                    elif sender==wcontent.strip():
-                        print("problem this guy is in our whitelist filter")
-                    else:
-                        #implicit deny
-                        print("placeholder")
-    
-    def add_quaratine(self, message_id):
-        body={
-            "addLabelIds": [self.label_id]
-        }
-        self.service.users().messages().modify(userId=self.email, id=message_id, body=body).execute()
+        try:
+            black_lists = open('blacklist.txt', 'r')
+            bcontent = black_lists.read()
+            white_lists = open('whitelist.txt', 'r')
+            wcontent = white_lists.read()
+            emails = self.service.users().messages().list(userId=self.email, labelIds='INBOX').execute()
+            blacklist_ids = []
+            
+            for messages in emails['messages']:
+                m = self.service.users().messages().get(userId=self.email, id=messages.get('id'), format='metadata').execute()
+                headers = (m.get("payload")).get("headers")
+                for header in headers:
+                    subject = None
+                    if header.get("name")=="Subject":
+                        subject = header.get("value")
+                        print(subject)
+                    if header.get("name")=="From":
+                        start = "<"
+                        end = ">"
+                        s = header.get("value")
+                        sender = s[s.find(start)+len(start):s.rfind(end)]
+                        print(sender)
+                        if sender==bcontent.strip():
+                            blacklist_ids.append(messages.get('id')) 
+                            self.audit_log("Filter", f'<{sender}> {subject}')
+                        elif sender==wcontent.strip():
+                            print("problem this guy is in our whitelist filter")
+                        # else:
+                        #     #implicit deny
+                        #     print("placeholder")
+                    
+            if blacklist_ids:
+                self.add_quarantine(blacklist_ids)
+
+        except HttpError as error:
+            print(f'An error occurred: {error}')        # SELF NOTE WRITE TO LOGGING
+            
+            
+    def add_quarantine(self, message_ids):
+        try:
+            body={
+                "addLabelIds": [self.label_id],
+                "ids": message_ids,
+                "removeLabelIds": ['INBOX']
+            }
+            self.service.users().messages().batchModify(userId=self.email, body=body).execute()
+        except HttpError as error:
+            print(f'An error occurred: {error}')        # SELF NOTE WRITE TO LOGGING
+
 
     def check_label(self, name):
-        labels= self.service.users().labels().list(userId=self.email).execute()
-        for label in labels.get("labels"):
-            if label.get("name") == name:
+        try:
+            labels= self.service.users().labels().list(userId=self.email).execute()
+            for label in labels.get("labels"):
+                if label.get("name") == name:
+                    return label.get("id")
+            else: 
+                label = self.service.users().labels().create(userId=self.email,body={"name":name}).execute()
                 return label.get("id")
-        else:
-            label = self.service.users().labels().create(userId=self.email,body={"name":name}).execute()
-            return label.get("id")
+        except HttpError as error:
+            print(f'An error occurred: {error}')        # SELF NOTE WRITE TO LOGGING
             
 
 app = Gmail_App("Test", "Account", "spamfilterbottester@gmail.com", "Quarantine")
 app.handler()
+
