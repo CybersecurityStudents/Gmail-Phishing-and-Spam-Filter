07b62a370027c1997422a7cc65122ef19addc935
Adithya Rajendran <adithyaraj@gmail.com>
2022-12-21 18:43:16 -0800

    Added wildcard functionality

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import re
+from fnmatch import fnmatch
 
 from Gmail_App import GmailApp, audit_log
 
@@ -14,7 +15,7 @@ def handler(app):
             sender = next((header.get("value") for header in headers if header["name"] == "From"), None)
             sender_email = (re.search('<(.*)>', sender)).group(1)
             for blocked_email in blocked_emails:
-                if sender_email == blocked_email.strip():
+                if fnmatch(sender_email,blocked_email.strip()):
                     blacklist_ids.append(messages.get('id'))
                     audit_log("Filter", f'{sender} : {subject}')
                     break
